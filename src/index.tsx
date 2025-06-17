968766a518186bf333ac5f19bba0eb6abed273ee
YellowJune <138018614+YellowJune@users.noreply.github.com>
2025-06-17 21:33:01 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // 스타일 파일 있다면 여기에 import
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
